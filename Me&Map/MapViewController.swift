e39edb1f0dec0fc842bbcd2b0e5cb32ad69f51a1
TA THANH <ta.thanh.03@gmail.com>
2018-12-24 13:56:24 +0100

    bla

--- Me&Map/MapViewController.swift
+++ Me&Map/MapViewController.swift
@@ -2,10 +2,22 @@ import UIKit
 import CoreLocation
 import MapKit
 
+extension String {
+    func mySlice(from: String, to: String) -> String? {
+        return (range(of: from)?.upperBound).flatMap { substringFrom in
+            (range(of: to, range: substringFrom..<endIndex)?.lowerBound).map { substringTo in
+                String(self[substringFrom..<substringTo])
+            }
+        }
+    }
+}
+
 class MapViewController: UIViewController {
     private let mapMode = UISegmentedControl(items: ["Map", "Satellite", "Mix"])
     private let map = MKMapView()
     private let textField = UITextField()
+    private var map_API_key = String()
+    
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -28,6 +40,9 @@ class MapViewController: UIViewController {
         //TextField
         textField.backgroundColor = UIColor.white
         textField.becomeFirstResponder()
+        
+        //DELETE THIS!!!!!!!!
+        map_API_key = ""
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -39,8 +54,6 @@ class MapViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.view = UIView(frame: UIScreen.main.bounds)
         self.view.backgroundColor = UIColor.yellow
-        
-        //actions
         
         //addsubview
         self.view.addSubview(textField)
@@ -72,11 +85,66 @@ class MapViewController: UIViewController {
         }
         if sender.selectedSegmentIndex == 2 {
             map.mapType = .hybrid
+            fetchData()
         }
     }
+    
+    @objc func fetchData() {
+        let stringText = textField.text
+        let stringReplaced = stringText!.replacingOccurrences(of: " ", with: "+")
+        let computed = "https://maps.googleapis.com/maps/api/geocode/xml?address=\(stringReplaced)&sensor=false&key=\(map_API_key)"
+        let myURL = URL(string: computed)
+        if myURL != nil {
+            let request = URLRequest(url: myURL!)
+            let config = URLSessionConfiguration.default
+            let session = URLSession(configuration: config)
+            let task = session.dataTask(with: request) {(data, resp, err) in 
+                DispatchQueue.main.async {
+                    if err != nil {
+                        let a = UIAlertController(title: "Error", message: err!.localizedDescription, preferredStyle: .alert)
+                        a.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                        self.present(a, animated: true, completion: nil)
+                    } else if data != nil {
+                        self.handleData(data!)
+                    } else {
+                        print("Empty data")
+                    }
+                }
+            }
+            task.resume()
+        } else {
+            print("URL failure!!!")
+        }
+    }
+    
+    func handleData(_ d: Data){
+        let s = String(decoding: d, as: UTF8.self)
+        let sStatus = s.mySlice(from: "<status>", to: "</status>")
+        let sAddress = s.mySlice(from: "<formatted_address>", to: "</formatted_address>")
+        let sLocation = s.mySlice(from: "<location>", to: "</location>")
+        let sLattitude = sLocation?.mySlice(from: "<lat>", to: "</lat>")
+        let sLongitude = sLocation?.mySlice(from: "<lng>", to: "</lng>")
+        print("DATA===================")
+        print(sStatus ?? "sStatus = NULL")
+        print(s)
+        print("=======================")
+        newCell()
+        updateMap()
+    }
+    
+    func newCell(){
+    }
+    
+    func updateMap() {
+        
+    }
+    
 }
 
 
 extension MapViewController : MKMapViewDelegate {
     
 }
+
+
+
